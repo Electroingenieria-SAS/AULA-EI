e45f1f376d95955098cd00ddb18616efb0edba9a
JEPTAC <j.perez@ei.com.co>
2026-09-21 11:22:53 -0500

    feat(learner): mount unified catalog and course router

--- player/src/main.jsx
+++ player/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CoursePlayer from './CoursePlayer.jsx'
+import LearnerApp from './LearnerApp.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CoursePlayer />
+    <LearnerApp />
   </React.StrictMode>,
 )
